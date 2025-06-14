13f5b850cfdc05203b181cba181a54d6cbee8be9
winzu44 <ftakuabai5000@gmail.com>
2025-06-15 03:37:46 +0900

    Fixed tab indent

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,7 +2,7 @@ return { -- override blink.cmp plugin
   "Saghen/blink.cmp",
   opts = {
     keymap = {
-      ["<Tab>"] = { "select_and_accept" },
+      ["<Tab>"] = { "select_and_accept", "fallback" },
     },
   },
 }
